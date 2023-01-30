218b1e371d49f6eb43c0eb410e3884c93b7e394d
northfacegawd <chuck5732590@gmail.com>
2023-01-30 20:16:12 +0900

    feat: 회원가입 완료시 login 페이지로 이동하도록 설정

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,12 +1,16 @@
 import {Alert} from 'react-native';
 import {SignUpData, signup} from '../requests/user';
 import {useMutation} from 'react-query';
+import {useNavigation} from '@react-navigation/native';
 
 const useSignUp = () => {
+  const navigation = useNavigation();
+
   const mutateFn = (data: SignUpData) => signup(data);
   return useMutation(['/user'], mutateFn, {
     onSuccess: () => {
       Alert.alert('알림', '회원가입 되었습니다.');
+      navigation.goBack();
     },
   });
 };
