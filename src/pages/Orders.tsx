59d47692ef7f6db2958220aad69988ceb7f07692
northfacegawd <chuck5732590@gmail.com>
2023-01-31 02:05:37 +0900

    feat: 오더 목록 뷰 구현

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {FlatList, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
+import {Order} from '../../types/order';
+import EachOrder from '../components/EachOrder';
 
 function Orders() {
+  const orders = useSelector((state: RootState) => state.order.orders);
+
+  const renderItem = useCallback(({item}: {item: Order}) => {
+    return <EachOrder item={item} />;
+  }, []);
+
   return (
     <View>
-      <Text>주문 페이지</Text>
+      <FlatList
+        data={orders}
+        keyExtractor={item => item.orderId}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
