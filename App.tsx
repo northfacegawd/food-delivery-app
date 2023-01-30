79bc2fcdd5baf7ebf88875dd36341cc40046861f
northfacegawd <chuck5732590@gmail.com>
2023-01-30 19:18:23 +0900

    chore: react query 적용

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Provider} from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 function App() {
+  const [queryClient] = useState(new QueryClient());
+
   return (
-    <Provider store={store}>
-      <AppInner />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <AppInner />
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
